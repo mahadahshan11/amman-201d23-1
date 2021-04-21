caad73128b8f76c83ebf506cb6d827459354999e
mahadahshan11 <maha.snape@gmail.com>
2021-04-21 15:05:50 +0300

    fixing the forth problem

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -84,7 +84,10 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+let summnation2 = sum(sumArr[0],sumArr[1])[0];
+summnation2 = sum(summnation2,sumArr[2])[0];
+let msg2 = sumArr[0]+ ','+ sumArr[2]+ 'was passed in as an array of numbers, and'+ summnation2 + 'is thier sum.';
+return[summnation2, msg2]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
